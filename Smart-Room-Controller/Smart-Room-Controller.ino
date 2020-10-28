18dd8dbbd5eec0dec5dc144a415a4e55534e18ef
unknown <abqcc3@gmail.com>
2020-10-28 09:17:45 -0600

    Currently compiling but, received failed to configure Ethernet on Serial monitor

--- Smart-Room-Controller/Smart-Room-Controller.ino
+++ Smart-Room-Controller/Smart-Room-Controller.ino
@@ -20,8 +20,22 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 EthernetClient client;
+bool status;
 const int trigPin = 5;
 const int echoPin = 4;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 void setup() {
@@ -45,37 +59,35 @@ void setup() {
     }
   Serial.println();
 }
-
-
-// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000); // Pause for 2 seconds
-
-  // Clear the buffer
-  display.clearDisplay();
-
-  // Draw a single pixel in white
-  display.drawPixel(10, 10, SSD1306_WHITE);
-
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
-  display.display();
-  delay(2000);
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
-  
-   testdrawstyles();    // Draw 'stylized' characters THIS IS NEEDED
-
-} //Is this an extra??
+//
+//
+//// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+//  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+//    Serial.println(F("SSD1306 allocation failed"));
+//    for(;;); // Don't proceed, loop forever
+//  }
+//               //CODE FOR OLED
+//  // Show initial display buffer contents on the screen --
+//  // the library initializes this with an Adafruit splash screen.
+//  display.display();
+//  delay(2000); // Pause for 2 seconds
+//
+//  // Clear the buffer
+//  display.clearDisplay();
+//
+//  // Draw a single pixel in white
+//  display.drawPixel(10, 10, SSD1306_WHITE);
+//
+//  // Show the display buffer on the screen. You MUST call display() after
+//  // drawing commands to make them visible on screen!
+//  display.display();
+//  delay(2000);
+//  // display.display() is NOT necessary after every single drawing command,
+//  // unless that's what you want...rather, you can batch up a bunch of
+//  // drawing operations and then update the screen all at once by calling
+//  // display.display(). These examples demonstrate both approaches...
+//  
+//   testdrawstyles();    // Draw 'stylized' characters THIS IS NEEDED
 
 void loop() {
   
