782362d3d9e37e39c60221db50bbf6bb5d4c117b
unknown <abqcc3@gmail.com>
2020-10-28 10:09:59 -0600

    Added code for bme senor and temp

--- Smart-Room-Controller/Smart-Room-Controller.ino
+++ Smart-Room-Controller/Smart-Room-Controller.ino
@@ -7,6 +7,8 @@
 
 #include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Ethernet.h>
@@ -17,6 +19,8 @@
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_BME280 bme; // use I2C interface       Errors without this code
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 EthernetClient client;
@@ -27,24 +31,12 @@ const int serialClock = 10;
 const int serialData = 12;
 const int teaButton = 13;
 const int fanButton = 16;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+float tempF;
 
 void setup() {
   //Open Serial Communication and wait for port to open: //This Code is not mine testing Ethernet connection
   Serial.begin(9600);
+  
   while (!Serial);
   Serial.println("Starting Program");
   //Start ethernet connection
@@ -63,6 +55,17 @@ void setup() {
     }
   Serial.println();
 }
+
+//    THIS IS CODE FOR bme senor communcation with serial monitor
+// //default settings
+//  status = bme.begin(0x76);
+//  
+//  if(status==false){
+//     Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+//     Serial.print(F("Fail"));
+//  }
+
+
 //
 //
 //// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -94,7 +97,9 @@ void setup() {
 //   testdrawstyles();    // Draw 'stylized' characters THIS IS NEEDED
 
 void loop() {
-  
-  // put your main code here, to run repeatedly:
+   // Reads temperature in fahrenheit
+   tempF =(tempC + 32)*5/9;
+   Serial.printf("%f\n",tempF);
+
 
 } //End Void Loop
